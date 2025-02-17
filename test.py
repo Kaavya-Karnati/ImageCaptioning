a70a2d3a19625fefe1be291da8d62bd8b8ab8fbe
Kaavya Karnati <kaavya.karnati@yottaasys.com>
2025-02-16 21:24:18 -0500

    Final Edits

--- test.py
+++ test.py
@@ -1,39 +1,3 @@
-# import torch
-# from models.encoder import EncoderCNN
-# from models.decoder import DecoderRNN
-# from PIL import Image
-# import torchvision.transforms as transforms
-
-# def test():
-#     # Load trained models
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     encoder = EncoderCNN(512).to(device)
-#     decoder = DecoderRNN(512, 512, 5000, 1).to(device)
-
-#     encoder.load_state_dict(torch.load("models/encoder.pth"))
-#     decoder.load_state_dict(torch.load("models/decoder.pth"))
-
-#     encoder.eval()
-#     decoder.eval()
-
-#     # Load and preprocess an example image
-#     # image = Image.open("data/test.jpg")
-#     image = torch.randn(1, 3, 224, 224).to(device)  # Random image tensor with shape (1, 3, 224, 224)
-#     transform = transforms.Compose([
-#         transforms.Resize((224, 224)),
-#         transforms.ToTensor()
-#     ])
-#     image = transform(image).unsqueeze(0).to(device)
-
-#     # Generate caption
-#     with torch.no_grad():
-#         features = encoder(image)
-#         caption = decoder.sample(features)  # You need to implement the `sample` method in the decoder
-#         print("Generated Caption:", caption)
-
-
-
-
 import torch
 from models.encoder import EncoderCNN
 from models.decoder import DecoderRNN
